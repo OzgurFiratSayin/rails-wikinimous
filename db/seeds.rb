1744daeb48ba1af184fd20873b6e0e4591a014a0
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-05-25 15:50:27 +0100

    do the challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    puts "creating an article"
+    article = Article.create(
+    title: Faker::Book.title,
+    content: Faker::Book.genre,
+    )
+    puts "finished making #{article.id}"
+  end
